9c4e79a6018941b5adfc35e1fd0bfe25fc531414
DUYLANG <hungvumix@gmail.com>
2025-02-28 14:52:31 +0700

    change as demand and format

--- src/components/layout/app.footer.tsx
+++ src/components/layout/app.footer.tsx
@@ -1,12 +1,16 @@
-
 const AppFooter = () => {
-    return (
-        <div className="text-center my-3">
-            Copyright © {new Date().getFullYear()} Hỏi Dân IT vs Eric. Made with <span style={{
-                color: "#e25555"
-            }}>♥</span> in VietNam
-        </div>
-    )
-}
+  return (
+    <div className="text-center my-3">
+      {new Date().getFullYear()} - Striving for growth{" "}
+      <span
+        style={{
+          color: "#e25555",
+        }}
+      >
+        ♥
+      </span>
+    </div>
+  );
+};
 
 export default AppFooter;
--- src/components/sections/social.media.tsx
+++ src/components/sections/social.media.tsx
@@ -1,34 +1,51 @@
-import { FaFacebook } from "react-icons/fa6";
-import { SiUdemy } from "react-icons/si";
-import { FaTiktok } from "react-icons/fa";
-import { SiYoutubeshorts } from "react-icons/si";
+import { FaFacebook, FaLinkedin } from "react-icons/fa6";
+import { SiGithub, SiGmail } from "react-icons/si";
 
 interface IProps {
-    youtube: string;
-    tiktok: string;
-    udemy: string;
-    facebook: string;
+  linkedin: string;
+  gmail: string;
+  github: string;
+  facebook: string;
 }
 const SocialMedia = (props: IProps) => {
-    const { youtube, tiktok, udemy, facebook } = props;
+  const { linkedin, gmail, github, facebook } = props;
 
-    return (
-        <div className="my-4 d-flex items-center gap-3">
-            <a href={youtube} target='_blank' className="highlight" title="Youtube Hỏi Dân IT">
-                <SiYoutubeshorts size={30} />
-            </a>
-            <a href={tiktok} target='_blank' className="highlight" title="Tiktok Hỏi Dân IT">
-                <FaTiktok size={30} />
-            </a>
-            <a href={udemy} target='_blank' className="highlight" title="Udemy Hỏi Dân IT">
-                <SiUdemy size={30} />
-            </a>
-            <a href={facebook} target='_blank' className="highlight" title="Facebook Hỏi Dân IT">
-                <FaFacebook size={30} />
-            </a>
-
-        </div>
-    )
-}
+  return (
+    <div className="my-4 d-flex items-center gap-3">
+      <a
+        href={linkedin}
+        target="_blank"
+        className="highlight"
+        title="My LinkedIn Profile"
+      >
+        <FaLinkedin size={30} />
+      </a>
+      <a
+        href={gmail}
+        target="_blank"
+        className="highlight"
+        title="Udemy Hỏi Dân IT"
+      >
+        <SiGmail size={30} />
+      </a>
+      <a
+        href={github}
+        target="_blank"
+        className="highlight"
+        title="Youtube Hỏi Dân IT"
+      >
+        <SiGithub size={30} />
+      </a>
+      <a
+        href={facebook}
+        target="_blank"
+        className="highlight"
+        title="Facebook Hỏi Dân IT"
+      >
+        <FaFacebook size={30} />
+      </a>
+    </div>
+  );
+};
 
 export default SocialMedia;
